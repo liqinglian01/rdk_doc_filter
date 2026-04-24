e0e3f390aea479b1949f35c2dc5b7925eab6f75e
liqinglian <105912712+liqinglian01@users.noreply.github.com>
2026-04-24 11:04:35 +0800

    Fix deploy errors
    Signed-off-by: liqinglian <105912712+liqinglian01@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: "https://liqinglian01.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/rdk_doc/",
+  baseUrl: "/rdk_doc_filter/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
